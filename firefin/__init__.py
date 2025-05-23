0775828eef4b3e4cdf354e22c6fb263c23550dfe
qiaobaochen <baochenqiao@gmail.com>
2025-05-23 14:12:05 +0800

    add evaluation module regression ignore nan

--- firefin/__init__.py
+++ firefin/__init__.py
@@ -6,5 +6,3 @@ from .data.gateway import fetch_data
 from .core.eva_utils import compute_forward_returns, compute_ic, compute_quantile_returns
 from .evaluation.industry.evaluator import Evaluator
 from .core.plot.plots import plt_ic, plt_quantile_cumulated_end_returns, plt_quantile_cumulative_returns
-from .evaluation.academia.AcaEvaluatorModel import AcaEvaluatorModel
-from .evaluation.academia.AcaEvaluatorModelComparison import AcaEvaluatorModelComparison
